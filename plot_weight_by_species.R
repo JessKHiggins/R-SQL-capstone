b06835492db8b1a7843d2c6a1b8ce8cc4f13a086
JessKHiggins <jessicakhiggins@gmail.com>
2016-04-12 15:55:51 -0400

    Fixed error with designating year in bash script.

--- plot_weight_by_species.R
+++ plot_weight_by_species.R
@@ -51,6 +51,8 @@ head(result)
 
 # plot the data and save to a png file
 ggplot(data=result, aes(x=species_id, y=weight))+
-  geom_boxplot()+theme_classic()+xlab("Species ID")+ylab("Weight (g)")
+  geom_boxplot()+theme_bw()+
+  xlab("Species ID")+ylab("Weight (g)")+
+  geom_jitter(color="darkblue", alpha=.3)
 outputfilename <- paste("spp_weight",year,".png", sep="")
 ggsave(outputfilename, width=5, height=5, units="in" )
